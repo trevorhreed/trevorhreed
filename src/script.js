cb47c2bdc6628b9909ec79bd57460d698f048e54
Trevor <trevorhreed@gmail.com>
2022-12-07 03:57:37 -0700

    updating all the things

---
+++ src/script.js
@@ -0,0 +1,56 @@
+const $ = (selector, context = document) => {
+  return Array.isArray(selector)
+    ? Array.from(context.querySelectorAll(selector[0] || ''))
+    : context.querySelector(selector)
+}
+
+$('#year').textContent = new Date().getFullYear()
+
+document.addEventListener('DOMContentLoaded', e => {
+
+  const $menu = $('#menu')
+  const $nav = $('#small-nav')
+  $menu.addEventListener('click', e => {
+    $menu.classList.toggle('active')
+    $nav.classList.toggle('active')
+  })
+
+  document.addEventListener('click', e => {
+    if (
+      e.target !== $menu
+      && e.target !== $nav
+      && !$menu.contains(e.target)
+    ) {
+      $menu.classList.remove('active')
+      $nav.classList.remove('active')
+    }
+  })
+
+  const $links = $(['[link]'])
+  const $sections = $(['main > section'])
+
+
+  const goToSection = url => {
+    $menu.classList.remove('active')
+    $nav.classList.remove('active')
+    $sections.forEach($section => {
+      const id = $section.getAttribute('id')
+      $section.classList.toggle('active', id === url)
+    })
+  }
+
+  $links.forEach($el => {
+    $el.addEventListener('click', e => {
+      e.preventDefault()
+      history.pushState(null, null, `#${$el.getAttribute('link')}`)
+      const url = $el.getAttribute('link')
+      goToSection(url)
+    })
+  })
+
+  if (window.location.hash) {
+    const url = window.location.hash.slice(1)
+    goToSection(url)
+  }
+
+})
